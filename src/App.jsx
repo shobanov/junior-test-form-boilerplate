cf6b69ff2374c0d61707877bef6632db34031405
Andrey Potyomkin <and_tem@icloud.com>
2022-12-09 10:48:07 +0300

    chore: Реализована основа для тестовго задания

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { ImageList } from './components';
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Тестовое задание</h1>
+      </header>
+      <main>
+        <section>
+          <ImageList />
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ImageList/ImageList.jsx
@@ -0,0 +1,9 @@
+import classes from './ImageList.module.css';
+
+export const ImageList = () => {
+  return (
+    <ul className={classes.list}>
+      <li>Image1</li>
+    </ul>
+  );
+};
