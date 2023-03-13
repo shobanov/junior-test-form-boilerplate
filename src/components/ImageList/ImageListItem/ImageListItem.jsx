699db4ec4bd8ab6a86c4b02357ad537344d441af
aleksej <shobanoff@gmail.com>
2023-03-13 21:11:41 +0300

    add handleImageError for incorrect url

--- src/components/ImageList/ImageListItem/ImageListItem.jsx
+++ src/components/ImageList/ImageListItem/ImageListItem.jsx
@@ -1,4 +1,5 @@
 import trash from '../../../assets/svg/trash.svg';
+import imageNotAvailable from '../../../assets/Img_not_available.png';
 
 import {
   Date,
@@ -9,21 +10,33 @@ import {
   Title,
 } from './styles';
 
-export const ImageListItem = (props) => {
+export const ImageListItem = ({
+  id,
+  title,
+  imgURL,
+  created_at,
+  description,
+  handleRemoveImageCard,
+}) => {
+  const handleImageError = (e) => {
+    e.target.src = imageNotAvailable;
+  };
+
   return (
     <ImageListItemContainer>
-      <ImageButton
-        src={trash}
-        onClick={() => props.handleRemoveImageCard(props.id)}
-      />
-      {props.title && <Title>{props.title}</Title>}
-      <Description>{props.description}</Description>
+      <ImageButton src={trash} onClick={() => handleRemoveImageCard(id)} />
+      {title && <Title>{title}</Title>}
+      <Description>{description}</Description>
       <Date>
         <span>Дата добавления: </span>
-        <time>{props.created_at}</time>
+        <time>{created_at}</time>
       </Date>
       <ImageContainer>
-        <img src={props.imgURL} alt="img" />
+        <img
+          src={imgURL ? imgURL : imageNotAvailable}
+          alt="img"
+          onError={handleImageError}
+        />
       </ImageContainer>
     </ImageListItemContainer>
   );
